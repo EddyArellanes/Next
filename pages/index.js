fa99794faacbcf391ff7d3af052c30f4ed94901e
Eddy Arellanes Bastida <eddyarellanes.developer@gmail.com>
2018-07-10 22:17:58 -0500

    Simple One Page finish

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,67 @@
+import 'isomorphic-fetch'
 export default class extends React.Component {
+    static async getInitialProps() {
+        let req = await fetch 
+        ('https://api.audioboom.com/channels/recommended')
+        //let get the attr channel of the object body
+        let { body: channels } = await req.json()
+        //channels: channels , when is equal de key-value name you can return like thah:
+        return {channels}
+    }
     render(){
-        return <h1>Hello World</h1>
+        // const channels = this.props.channels, destructuring below, again :D
+        const { channels} = this.props
+        return <div>
+                <header>Podcasts</header>                
+                <div className="channels">
+                    { channels.map((channel) => (
+                    <a className="channel" key={ channel.id }>
+                        <img src={ channel.urls.logo_image.original } alt=""/>
+                        <h2>{ channel.title }</h2>
+                    </a>
+                    )) }
+                </div>
+                <style jsx>{`
+                    header {
+                        color: #fff;
+                        background: #8756ca;
+                        padding: 15px;
+                        text-align: center;
+                    }     
+                    .channels {
+                        display: grid;
+                        grid-gap: 15px;
+                        padding: 15px;
+                        grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+                    }
+                    a.channel {
+                        display: block;
+                        margin-bottom: 0.5em;
+                        color: #333;
+                        text-decoration: none;
+                    }
+                    .channel img {
+                        border-radius: 3px;
+                        box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+                        width: 100%;
+                    }
+                    h2 {
+                        padding: 5px;
+                        font-size: 0.9em;
+                        font-weight: 600;
+                        margin: 0;
+                        text-align: center;
+                    }           
+
+                `}</style>
+
+                 <style jsx global>{`
+                    body {
+                    margin: 0;
+                    font-family: system-ui;
+                    background: white;
+                    }
+                `}</style>
+            </div>
     }
 }
